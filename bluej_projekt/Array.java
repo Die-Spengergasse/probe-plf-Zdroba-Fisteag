5c2a6c5ef60e4826b83d1f5970836f7bcecb8f66
ZDR240654 <ZDR240654@plf.spengergasse.at>
2025-10-20 15:04:11 +0200

    autocommit

--- bluej_projekt/Array.java
+++ bluej_projekt/Array.java
@@ -16,13 +16,16 @@ public class Array {
 
     // hier wird das größte Element des Arrays gesucht
     public static int arrayMax(int[] arr) {
-        int rv = Integer.MAX_VALUE;
+        int rv = Integer.MIN_VALUE;
+        for (int i = rv; i < Integer.MIN_VALUE; i++) {
+            
+        }
         return rv;
     }
 
     // hier wird das kleinste Element des Arrays gesucht
     public static int arrayMin(int[] arr) {
-        int rv = Integer.MIN_VALUE;
+        int rv = Integer.MAX_VALUE;
         return rv;
     }
 
